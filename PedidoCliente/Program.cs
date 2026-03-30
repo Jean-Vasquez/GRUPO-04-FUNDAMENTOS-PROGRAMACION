9e4259be7fe425c12acbc6aebfa9f5c39656e0aa
Augusto Asto <jeisseras23@gmail.com>
2026-03-29 21:58:57 -0500

    jaas: agrego esqueleto del programa

--- PedidoCliente/Program.cs
+++ PedidoCliente/Program.cs
@@ -1,28 +1,48 @@
-﻿String nombreCliente = "";
-bool nombreValido = false;
-
-while (!nombreValido)
+﻿using System;
+class Program
 {
+   static void Main()
+{
+    IngresoDatos();
+
+} 
+
+
+static void IngresoDatos()
+{
+        String nombreCliente = "";
+        bool nombreValido = false;
+        while (!nombreValido)
+        {
     /*Solicitud de nombre al usuario, si posiblemente llegara nulo, el valor se asigna por defecto vacío, luego se valida que no esté vacío
      con el fin de evitar una incorrecta inserción de datos*/
-    Console.WriteLine("Por favor, escribe tu nombre");
-    nombreCliente = Console.ReadLine() ?? "";
-    if (nombreCliente == "")
-    {
-        Console.WriteLine("El nombre no puede estar vacío, por favor ingresa un nombre válido");
-    }
-    else
-
-
-    {   //Se valida que el nombre no tenga números, sino, vuelve a solicitar el nombre al usuario
-        if (nombreCliente.Any(char.IsDigit))
+        Console.WriteLine("Por favor, escribe tu nombre");
+        nombreCliente = Console.ReadLine() ?? "";
+        if (nombreCliente == "")
         {
-            Console.WriteLine("El nombre no puede contenedor números, por favor ingresa un nombre válido");
-            nombreCliente = "";
+            Console.WriteLine("El nombre no puede estar vacío, por favor ingresa un nombre válido");
         }
         else
-        {
-            nombreValido = true;
+        {   //Se valida que el nombre no tenga números, sino, vuelve a solicitar el nombre al usuario
+            if (nombreCliente.Any(char.IsDigit))
+            {
+                Console.WriteLine("El nombre no puede contenedor números, por favor ingresa un nombre válido");
+                nombreCliente = "";
+            }
+            else
+            {
+                nombreValido = true;
+            }
         }
+    }
+}
+
+static void Calculo()
+    {
+        
+    }
+static void ImpresionResultados()
+    {
+        
     }
 }
