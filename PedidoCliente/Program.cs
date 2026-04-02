fc16fbfdb3310b56b5b7972f50290570b994558a
Jean Vasquez <jeanpool5921@gmail.com>
2026-04-02 15:27:01 -0500

    Se muestran y toman datos del producto.

--- PedidoCliente/Program.cs
+++ PedidoCliente/Program.cs
@@ -9,10 +9,25 @@ class Program
     }
 
 
+    //JEAN
+
     static void IngresoDatos()
     {
         String nombreCliente = "";
         bool nombreValido = false;
+        int seleccionCategoria = 0;
+        int seleccionProducto = 0;
+        bool productoValido = false;
+        List<String> productoSeleccionado = new List<string>();
+        List<int> Cantidad = new List<int>();
+        List<double> Precio = new List<double>();
+        Dictionary<int, List<string>> Productos = new Dictionary<int, List<string>>()
+        {
+            { 1, new List<string> { "Arroz", "Aceite", "Azúcar", "Fideos", "Lentejas" }},
+            { 2, new List<string> { "Leche", "Yogurt", "Queso", "Mantequilla", "Crema de leche" }},
+            { 3, new List<string> { "Agua mineral", "Gaseosa", "Jugo de fruta", "Cerveza", "Vino" }},
+            { 4, new List<string> { "Papas fritas", "Galletas", "Chocolates", "Maní", "Popcorn" }}
+        };
 
         while (!nombreValido)
         {
@@ -37,7 +52,83 @@ class Program
                 }
             }
         }
+
+        while (!productoValido)
+        {
+            seleccionCategoria = mostrarCategoria();
+            seleccionProducto = mostrarProductos(Productos, seleccionCategoria);
+
+            if (seleccionProducto == 0)
+            {
+                Console.WriteLine("Debe seleccionar un producto válido, por favor intente nuevamente");
+            }
+            else
+            {
+                Console.Write("Ingrese la cantidad del producto: ");
+                int cantidad = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el precio del producto: ");
+                double precio = double.Parse(Console.ReadLine());
+
+                anadirProducto(Productos, seleccionCategoria, seleccionProducto, cantidad, precio, productoSeleccionado, Cantidad, Precio);
+                if (productoSeleccionado.Count == 5)
+                {
+
+                    productoValido = true;
+                }
+            }
+
+        }
+
     }
+
+    static int mostrarProductos(Dictionary<int, List<string>> Productos, int seleccion)
+    {
+        int productoValidar = 0;
+
+        if (Productos.ContainsKey(seleccion))
+        {
+            var prod = Productos[seleccion];
+            foreach (var producto in prod)
+            {
+                int numeroProducto = prod.IndexOf(producto) + 1;
+                Console.WriteLine($"{numeroProducto}. {producto}");
+            }
+            return productoValidar = Int32.Parse(Console.ReadLine());
+        }
+        else
+        {
+            Console.WriteLine("La categoría seleccionada no es válida");
+            return productoValidar;
+        }
+    }
+
+    static int mostrarCategoria()
+    {
+        List<String> categorias = new List<string> { "Abarrotes", "Lacteos", "Bebidas", "Snacks" };
+        int categoriaValidar = 0;
+
+        Console.WriteLine("Digíte el número de una de las categorías para acceder a los producto:");
+        foreach (string categoria in categorias)
+        {
+            int numeroCategoria = categorias.IndexOf(categoria) + 1;
+            Console.WriteLine($"{numeroCategoria}. {categoria}");
+        }
+        return categoriaValidar = Int32.Parse(Console.ReadLine());
+    }
+
+    static void anadirProducto(Dictionary<int, List<string>> Productos, int categoria, int producto, int cantidad, double precio, List<string> pSeleccionado, List<int> pCantidad, List<double> pPrecio)
+    {
+        string nombreArticulo = Productos[categoria][producto - 1];
+
+        pSeleccionado.Add(nombreArticulo);
+        pCantidad.Add(cantidad);
+        pPrecio.Add(precio);
+
+    }
+
+    //JEAN 
+
 
     static void Calculo() //se realisa el calculo de los productos//
     {
